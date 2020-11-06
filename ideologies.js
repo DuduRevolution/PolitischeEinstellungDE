b89d5d1fd21b957c1e8d55030b436177762137a9
DuduRevolution <74057509+DuduRevolution@users.noreply.github.com>
2020-11-06 16:20:26 +0100

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,470 @@
+ideologies = [
+    {
+        "name": "Demokratischer Sozialismus",
+        "stats": {
+            "cap": 100,
+            "nat": 50,
+            "dem": 100,
+            "fff": 90
+        }
+    },
+    {
+        "name": "Marxismus",
+        "stats": {
+            "cap": 100,
+            "nat": 70,
+            "dem": 80,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Antinationaler Marxismus",
+        "stats": {
+            "cap": 100,
+            "nat": 100,
+            "dem": 60,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Traditioneller Sozialismus",
+        "stats": {
+            "cap": 100,
+            "nat": 70,
+            "dem": 40,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Leninismus",
+        "stats": {
+            "cap": 100,
+            "nat": 30,
+            "dem": 30,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Maoismus",
+        "stats": {
+            "cap": 100,
+            "nat": 40,
+            "dem": 20,
+            "fff": 70
+        }
+    },
+    {
+        "name": "Stalinismus",
+        "stats": {
+            "cap": 100,
+            "nat": 20,
+            "dem": 0,
+            "fff": 60
+        }
+    },
+    {
+        "name": "Konservativer Sozialismus",
+        "stats": {
+            "cap": 100,
+            "nat": 50,
+            "dem": 30,
+            "fff": 30
+        }
+    },
+    {
+        "name": "Nationaler Sozialismus",
+        "stats": {
+            "cap": 80,
+            "nat": 30,
+            "dem": 30,
+            "fff": 70
+        }
+    },
+    {
+        "name": "Sozialistischer Gottesstaat",
+        "stats": {
+            "cap": 80,
+            "nat": 50,
+            "dem": 30,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Religiöser Sozialismus",
+        "stats": {
+            "cap": 80,
+            "nat": 50,
+            "dem": 70,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Posthumanistischer Sozialismus",
+        "stats": {
+            "cap": 80,
+            "nat": 50,
+            "dem": 50,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Patriotischer Sozialismus",
+        "stats": {
+            "cap": 80,
+            "nat": 20,
+            "dem": 50,
+            "fff": 70
+        }
+    },
+    {
+        "name": "Revolutionäre Sozialdemokratie",
+        "stats": {
+            "cap": 80,
+            "nat": 80,
+            "dem": 80,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Radikale Sozialdemokratie",
+        "stats": {
+            "cap": 80,
+            "nat": 50,
+            "dem": 100,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Gemäßigter Nationalismus",
+        "stats": {
+            "cap": 60,
+            "nat": 40,
+            "dem": 30,
+            "fff": 70
+        }
+    },
+    {
+        "name": "Destruktiver Nationalismus",
+        "stats": {
+            "cap": 60,
+            "nat": 40,
+            "dem": 30,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Konservative Sozialdemokratie",
+        "stats": {
+            "cap": 60,
+            "nat": 50,
+            "dem": 50,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Sozialdemokratie",
+        "stats": {
+            "cap": 60,
+            "nat": 60,
+            "dem": 60,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Sozialer Populismus",
+        "stats": {
+            "cap": 60,
+            "nat": 60,
+            "dem": 50,
+            "fff": 30
+        }
+    },
+    {
+        "name": "Gemäßigter Sozialismus",
+        "stats": {
+            "cap": 60,
+            "nat": 70,
+            "dem": 60,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Ökologischer Anarchismus",
+        "stats": {
+            "cap": 60,
+            "nat": 80,
+            "dem": 60,
+            "fff": 100
+        }
+    },
+    {
+        "name": "Demokratischer Anarchismus",
+        "stats": {
+            "cap": 60,
+            "nat": 50,
+            "dem": 100,
+            "fff": 70
+        }
+    },
+    {
+        "name": "Nationalistische Diktatur",
+        "stats": {
+            "cap": 50,
+            "nat": 20,
+            "dem": 0,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Internationale Diktatur",
+        "stats": {
+            "cap": 50,
+            "nat": 80,
+            "dem": 0,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Posthumanistische Autokratie",
+        "stats": {
+            "cap": 60,
+            "nat": 60,
+            "dem": 20,
+            "fff": 70
+        }
+    },
+    {
+        "name": "Konservatismus",
+        "stats": {
+            "cap": 50,
+            "nat": 50,
+            "dem": 50,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Sozialer Konservatismus",
+        "stats": {
+            "cap": 50,
+            "nat": 60,
+            "dem": 60,
+            "fff": 60
+        }
+    },
+    {
+        "name": "Destruktiver Anarchismus",
+        "stats": {
+            "cap": 50,
+            "nat": 50,
+            "dem": 100,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Rechtspopulismus",
+        "stats": {
+            "cap": 40,
+            "nat": 30,
+            "dem": 30,
+            "fff": 30
+        }
+    },
+    {
+        "name": "Religiöser Konservatismus",
+        "stats": {
+            "cap": 40,
+            "nat": 40,
+            "dem": 50,
+            "fff": 30
+        }
+    },
+    {
+        "name": "Religiöser Konservatismus",
+        "stats": {
+            "cap": 40,
+            "nat": 40,
+            "dem": 40,
+            "fff": 10
+        }
+    },
+    {
+        "name": "Humanistische Sozialdemokratie",
+        "stats": {
+            "cap": 60,
+            "nat": 70,
+            "dem": 80,
+            "fff": 70
+        }
+    },
+    {
+        "name": " Demokratischer Liberalismus",
+        "stats": {
+            "cap": 40,
+            "nat": 60,
+            "dem": 80,
+            "fff": 60
+        }
+    },
+    {
+        "name": "Anarchistischer Kapitalismus",
+        "stats": {
+            "cap": 40,
+            "nat": 50,
+            "dem": 100,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Neo-Nationalsozialismus",
+        "stats": {
+            "cap": 40,
+            "nat": 0,
+            "dem": 0,
+            "fff": 5
+        }
+    },
+    {
+        "name": "Autokratie",
+        "stats": {
+            "cap": 50,
+            "nat": 20,
+            "dem": 20,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Faschistische Autokratie",
+        "stats": {
+            "cap": 40,
+            "nat": 20,
+            "dem": 20,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Kapitalistische Autokratie",
+        "stats": {
+            "cap": 20,
+            "nat": 20,
+            "dem": 20,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Kapitalistischer Konservatismus",
+        "stats": {
+            "cap": 30,
+            "nat": 40,
+            "dem": 40,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Neo-Liberalismus",
+        "stats": {
+            "cap": 30,
+            "nat": 30,
+            "dem": 50,
+            "fff": 60
+        }
+    },
+    {
+        "name": "Gemäßigter Liberalismus",
+        "stats": {
+            "cap": 30,
+            "nat": 60,
+            "dem": 60,
+            "fff": 80
+        }
+    },
+    {
+        "name": "Kapitalistischer Populismus",
+        "stats": {
+            "cap": 20,
+            "nat": 30,
+            "dem": 20,
+            "fff": 40
+        }
+    },
+    {
+        "name": "Nationaler Kapitalismus",
+        "stats": {
+            "cap": 20,
+            "nat": 50,
+            "dem": 30,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Neo-Konservatismus",
+        "stats": {
+            "cap": 20,
+            "nat": 20,
+            "dem": 40,
+            "fff": 20
+        }
+    },
+    {
+        "name": "Fundamentalismus",
+        "stats": {
+            "cap": 20,
+            "nat": 30,
+            "dem": 30,
+            "fff": 5
+        }
+    },
+    {
+        "name": "Liberaler Kapitalismus",
+        "stats": {
+            "cap": 20,
+            "nat": 50,
+            "dem": 80,
+            "fff": 60
+        }
+    },
+    {
+        "name": "Liberaler Anarchismus",
+        "stats": {
+            "cap": 20,
+            "nat": 50,
+            "dem": 100,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Kapitalismus",
+        "stats": {
+            "cap": 10,
+            "nat": 50,
+            "dem": 90,
+            "fff": 40
+        }
+    },
+    {
+        "name": "Dystopischer Ultra-Kapitalismus",
+        "stats": {
+            "cap": 0,
+            "nat": 30,
+            "dem": 0,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Ultra-Kapitalismus",
+        "stats": {
+            "econ": 0,
+            "nat": 40,
+            "dem": 50,
+            "fff": 50
+        }
+    },
+    {
+        "name": "Anarcho-Kapitalismus",
+        "stats": {
+            "cap": 0,
+            "nat": 50,
+            "dem": 100,
+            "fff": 50
+        }
+    }
+];
